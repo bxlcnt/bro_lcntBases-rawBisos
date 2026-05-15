340b9339f1155be71522bd5697596600135be377
Mohsen Banan <form@mohsen.banan.1.byname.net>
2026-05-14 21:18:27 -0700

    Added source-info for bisos.

---
+++ lcnt/lgpc/rawBisos/SOURCE-INFO/lcntSourceInfo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+lcntSource_origin="rawBisos"
+lcntSource_author1="REGISTRY/organization/rawBisos"
+lcntSource_pubDestUrls="rawbisos.net"
+lcntSource_organization=""
+lcntSource_bystarUid="notYet"
